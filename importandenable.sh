20b094e1b69b3fbf5f013dff8ec5429615a387fe
Super-User <jmalcolm@uraeus.com>
2013-12-29 15:57:04 +0000

    new instructions for importing and enabling tahoe

---
+++ importandenable.sh
@@ -0,0 +1,6 @@
+svccfg  import tahoeserver.xml 
+svcadm enable tahoe-lafs-server
+svcs tahoe-lafs-server
+#STATE          STIME    FMRI
+#online          1:57:16 svc:/site/tahoe-lafs-server:default
+
